2e4a9ea0d974236b370bc9eb7a7d0ebb61ba0aa5
amgboye-cloud <amgboye@gmail.com>
2025-12-24 19:18:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function show(){
+    const menu = document.getElementById('mobile-menuBar');
+    menu.classList.toggle('show');
+}
+
+function hide(){
+     const menu = document.getElementById('mobile-menuBar');
+     menu.classList.remove('show');   
+}
